441772b790daa356895a493a0060b3a8b625b6f6
dptanedo <dmptanedo@gmail.com>
2017-09-22 18:30:28 +0800

    Comments and remove redundant parentheses
    + added comments for each phase
    
    + removed redundant parentheses at message.sid

--- send_time_to_go_home.py
+++ send_time_to_go_home.py
@@ -10,14 +10,15 @@ date_now = datetime.now()
 with open('credentials.json') as json_file:
     json_dict = json.load(json_file)
 
+# google request phase
 google_client = googlemaps.Client(key=json_dict['google_key'])
-
 results = google_client.directions(json_dict['departure_point'], json_dict['arrival_point'],
                                    mode='driving', departure_time=date_now)
 
 time_before_arrival = results[0]['legs'][0]['duration_in_traffic']['text']
 print(time_before_arrival)
 
+# web browser phase
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get(json_dict['maps_link'])
@@ -26,10 +27,11 @@ driver.get(json_dict['maps_link'])
 client = Client(json_dict['account_sid'],
                 json_dict['auth_token'])
 
+# sending of message to receiving number
 message = client.messages.create(
     to=json_dict['receiving_number'],
     from_=json_dict['twilio_generated_number'],
     body='Mahal uwi nako in {}'.format(time_before_arrival))
 
-if (message.sid):
+if message.sid:
     print('Sending of text message successful!')
