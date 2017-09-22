5d7bcbf33301a77ace808617277c4c8541916c72
dptanedo <dmptanedo@gmail.com>
2017-09-22 15:46:26 +0800

    README.md     added requirements and instructions
    send_time_to_go_home.py
        removed static values from the script
        added text messaging feature via Twilio
        added browser pop up for the traffic visuals
    
    credentials.json
        added credentials for Twilio
        added keys for send_time_to_go_home

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # personal_project
+
+## send_time_to_go_home.py
+
+> queries the amount of time need to navigate the departure and arrival point
+> and send it via text using Twilio;
+> also opens a web page to show the route Google maps chose
+
+## Requirements
+**Packages**
++ googlemaps 
++ Twilio
++ selenium
+
+**Credentials**
++ google API key
++ Twilio authorization sid, token
+
+**User defined values**
++ Twilio number and Twilio test number (your number)
+
+**Instructions**
+> Get the credentials needed from these sites 
+>> Google API Key for Google Maps [Google API](https://developers.google.com/maps/documentation/javascript/get-api-key)
+
+>> Authorization id, token and generated number from [Twilio API](https://www.twilio.com/)
+
+> Generate a short URL of Google Maps using your starting point and destination point 
+  [For Selenium](https://goo.gl/maps/eVYQMnLSkVx)
+  
+> Populate the credentials.json with your own credentials and data
--- send_time_to_go_home.py
+++ send_time_to_go_home.py
@@ -1,18 +1,9 @@
 
 from datetime import datetime
-from pync import Notifier
-from pprint import pprint
+from selenium import webdriver
+from twilio.rest import Client
 import googlemaps
 import json
-import os
-
-DEPARTURE_POINT = '2F LaunchPad, corner Streets, Sheridan, Mandaluyong, Metro Manila'
-ARRIVAL_POINT = 'SM Megamall, Epifanio de los Santos Ave, Ortigas Center, Mandaluyong, Metro Manila'
-MAPS_LINK = 'https://www.google.com.ph/maps/dir/2F+LaunchPad,+corner+Streets,+Sheridan,+Mandaluyong,' + \
-            '+Metro+Manila/SM+Megamall,+Epifanio+de+los+Santos+Avenue,+Mandaluyong/@14.579816,121.0488233' + \
-            ',16z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3397c843d548c681:0x45ee89aca3b50cea!2m2!1d121.' + \
-            '0535383!2d14.5741085!1m5!1m1!1s0x3397c815ec1c52e7:0x49e80caaf287652b!2m2!1d121.0564561!' + \
-            '2d14.583439?hl=en'
 
 date_now = datetime.now()
 
@@ -21,17 +12,24 @@ with open('credentials.json') as json_file:
 
 google_client = googlemaps.Client(key=json_dict['google_key'])
 
-# noinspection PyUnresolvedReferences
-results = google_client.directions(DEPARTURE_POINT, ARRIVAL_POINT, mode='driving', departure_time=date_now)
-
-#pprint(results)
+results = google_client.directions(json_dict['departure_point'], json_dict['arrival_point'],
+                                   mode='driving', departure_time=date_now)
 
 time_before_arrival = results[0]['legs'][0]['duration_in_traffic']['text']
+print(time_before_arrival)
 
-Notifier.notify(time_before_arrival, title='Traffic Status', open=MAPS_LINK)
-print 'hhaha'
-Notifier.remove(os.getpid())
-print '-----'
-#Notifier.list(os.getpid())
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(json_dict['maps_link'])
 
-exit()
+
+client = Client(json_dict['account_sid'],
+                json_dict['auth_token'])
+
+message = client.messages.create(
+    to=json_dict['receiving_number'],
+    from_=json_dict['twilio_generated_number'],
+    body='Mahal uwi nako in {}'.format(time_before_arrival))
+
+if (message.sid):
+    print('Sending of text message successful!')
